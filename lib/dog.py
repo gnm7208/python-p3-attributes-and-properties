0a823760ebd91b34578f261a37be8a892e841e2d
George <gnm7208@gmail.com>
2025-11-15 01:43:29 +0300

    fix: validate dog name length and type in constructor

--- lib/dog.py
+++ lib/dog.py
@@ -12,4 +12,21 @@ APPROVED_BREEDS = [
 ]
 
 class Dog:
+    def __init__(self,name="Simba", breed="Korokoro"):
+        self.name = name
+
+    if name == type(str) and (char(range(1,26)) in name):
+        print("Valid dog name")
+    else:
+        print("Name must be string between 1 and 25 characters")
+
+    def breed(self, breed):
+        if breed in APPROVED_BREEDS:
+            self.breed = breed
+            print(f"{self.name} is a {self.breed}")
+        else:
+            print(f"Breed '{breed}' is not approved.")
+        
     pass
+
+    
