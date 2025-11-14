f6ca584aaebf1711925eb8f9d337301cb7afbc82
George <gnm7208@gmail.com>
2025-11-15 01:55:09 +0300

    fix: correct name validation logic in Person constructor

--- lib/person.py
+++ lib/person.py
@@ -16,4 +16,19 @@ APPROVED_JOBS = [
 ]
 
 class Person:
+    def __init__(self, name="Alex", job="Admin"):
+        self.name = name
+
+        if name == type(str) and (char(range(1,26)) in name):
+            return name.totitle()
+        else:
+            print("Name must be string between 1 and 25 characters")
+
+        if job in APPROVED_JOBS:
+            self.job = job
+        else:
+            print("Job must be in list of approved jobs.")
+        
+    
+
     pass
